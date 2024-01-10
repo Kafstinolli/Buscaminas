2e9b2f69362fdbad0859e9dfb5dfd77bd072ca22
Andres Gomez <agomez47@uan.edu.co>
2024-01-09 22:21:12 -0500

    funcion limpiar tablero

---
+++ Buscaminas/Buscaminas.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+#include <climits>
+#include <vector>
+
+using namespace std;
+
+int columnasJuego;
+int filasJuego;
+int minasJuego;
+int banderasRestantesJuego;
+bool activarTruco;
+
+int juegosGanados;
+int juegosPerdidos;
+int juegosCancelados;
+vector<vector<string>>contenidoEstadisticas;
+
+string estadisticas[3][2];
+
+char tableroVisible[20][20];
+char tableroInvisible[20][20];
+
+
+void calcularMinasAlrededor();
+int verificarPartida();
+void limpiarTablero();
+
+int main() {
+
+
+
+
+
+}
+
+void calcularMinasAlrededor() {
+
+	int contadorMinas;
+	
+	for (int fila = 0; fila < filasJuego; fila++) {
+		for (int columna = 0; columna < columnasJuego; columna++) {
+			if (tableroInvisible[fila][columna] = 'M') {
+				contadorMinas = 0;
+
+				for (int i = -1; i <= 1; i++) {
+					for (int j = -1; j <= 1; j++) {
+						if (i == 0 && j == 0)
+							continue;
+
+						int verificarFila = fila + i;
+						int verificarColumna = columna + j;
+
+						if (verificarFila < 0 || verificarFila >= filasJuego || verificarColumna < 0 || verificarColumna >= columnasJuego)
+							continue;
+						if (tableroInvisible[verificarFila][verificarColumna] == 'M')
+							contadorMinas++;
+
+					}
+				}
+				tableroInvisible[fila][columna] = contadorMinas + 48;
+
+			}
+
+		}
+	}
+}
+
+int verificarPartida() {
+
+	for (int fila = 0; fila < filasJuego; fila++) {
+		for (int columna = 0; columna < columnasJuego; columna++) {
+			if (tableroVisible[fila][columna] == 'X')
+				return 0;
+		}
+	}
+	for (int fila = 0; fila < filasJuego; fila++) {
+		for (int columna = 0; columna < columnasJuego; columna++) {
+			if ((tableroVisible[fila][columna] != tableroInvisible[fila][columna]) && tableroInvisible[fila][columna] != 'M')
+				return 1;
+		}
+	}
+	return 2;
+}
+
+void limpiarTablero() {
+
+	for (int fila = 0; fila < 20; fila++) {
+		for (int columna = 0; columna < 20; columna++) {
+			tableroVisible[fila][columna] = '.';
+			tableroInvisible[fila][columna] = '.';
+		}
+	}
+}
